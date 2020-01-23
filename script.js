1b1e56e979e9f675116050b1287ff9e9dea00ce0
CaptainO5 <48147068+CaptainO5@users.noreply.github.com>
2020-01-23 23:47:20 +0530

    removed useless comments and seperated styles and script

---
+++ script.js
@@ -0,0 +1,35 @@
+AOS.init();
+$("#spy").scrollspy();
+
+window.onscroll = function() {scrollFunction()};
+var mybutton = document.getElementById("myBtn");
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+var cnt = document.getElementsByClassName("navbar-nav")[0];
+
+var tween = new TimelineLite();
+
+tween.add(
+    TweenLite.to(".navbar-nav", .5, {
+        x: cnt.offsetLeft,
+        ease: Power1.easeInOut
+    })
+);
+
+const controller = new ScrollMagic.Controller();
+const scene = new ScrollMagic.Scene({
+    duration: 800,
+    triggerHook: 0,
+}).setTween(tween)
+.addTo(controller);
