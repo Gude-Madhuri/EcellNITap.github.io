347f1dcb7ba41a471d6ba31e90bee8f26084f8bb
CaptainO5 <48147068+CaptainO5@users.noreply.github.com>
2020-02-18 22:07:24 +0530

    Abstraction.. Gallery is fully responsive

--- script.js
+++ script.js
@@ -33,3 +33,71 @@ const scene = new ScrollMagic.Scene({
     triggerHook: 0,
 }).setTween(tween)
 .addTo(controller);
+
+window.onresize = function(){
+
+}
+
+function fadeOut() {
+
+    TweenMax.to(".screen", 2, {
+        y: -400,
+        opacity: 0,
+        ease: Power2.easeInOut,
+        delay: 2
+    });
+
+    TweenMax.from(".overlay", 2, {
+        ease: Power2.easeInOut
+    });
+
+    TweenMax.to(".overlay", 2, {
+        delay: 2.6,
+        top: "-110%",
+        ease: Expo.easeInOut
+    });
+
+    TweenMax.to(".overlay-2", 1.5, {
+        delay: 3,
+        top: "-110%",
+        ease: Expo.easeInOut
+    });
+
+    TweenMax.to(".navbar", 2, {
+        opacity: 1,
+        delay: 3.2,
+        ease: Power2.easeInOut
+    });
+
+    TweenMax.from("#ei", .3, {
+        width: "30vw",
+        delay: 5,
+        opacity: 0, 
+        ease: Power2.Expo
+    })
+
+    TweenMax.from("#na", .3, {
+        width: "30vw",
+        opacity: 0,
+        delay: 5,
+        ease: Power2.Expo
+    })
+
+}
+
+document.getElementsByClassName("containerLand")[0].style.display = "None";
+if(window.scrollY==0){
+    document.getElementsByClassName("containerLand")[0].style.display = "block";
+    fadeOut();
+    window.setTimeout(function(){
+        document.getElementsByTagName("body")[0].style.overflowY="hidden";
+    }, 1)
+    window.setTimeout(function(){
+        document.getElementsByTagName("body")[0].style.overflow="auto";
+    }, 3600) 
+} else{
+    TweenMax.to(".navbar", 2, {
+        opacity: 1,
+        ease: Power2.easeInOut
+    });
+}
