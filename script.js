6d3d5faeaccf02c081906a02a50e1eeb96f749ba
CaptainO5 <48147068+CaptainO5@users.noreply.github.com>
2020-02-21 18:34:01 +0530

    About Modal..

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 AOS.init();
 $("#spy").scrollspy();
 
+var modal = document.getElementById("abtModal");
+
+var btn = document.getElementById("mdlBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 window.onscroll = function() {scrollFunction()};
 var mybutton = document.getElementById("myBtn");
 function scrollFunction() {
